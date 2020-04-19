6cfc01612bc05924f5cf1a856b17ddca0b8322ed
dengxw <dengxw@rd.netease.com>
2020-04-19 20:12:27 +0800

    函数式编程接口

--- src/main/java/com/dxw/function/FunctionTest.java
+++ src/main/java/com/dxw/function/FunctionTest.java
@@ -1,7 +1,5 @@
 package com.dxw.function;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.*;
 
 public class FunctionTest {
--- src/main/java/com/dxw/guava/OptionalTest.java
+++ src/main/java/com/dxw/guava/OptionalTest.java
@@ -1,18 +1,34 @@
 package com.dxw.guava;
 
+import com.google.common.collect.Sets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Stream;
 
 public class OptionalTest {
-
-
 
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
         list.add(3);
-        Optional.ofNullable(list).map(List::stream);
+        Optional.ofNullable(list).map(List::stream).orElseGet(Stream::empty).forEach(System.out::println);
+
+        Set<Integer> set1 = Sets.newHashSet(1,2,3);
+        Set<Integer> set2 = Sets.newHashSet(2,3,4);
+        Set<Integer> set3 = Sets.union(set1,set2);
+        Set<Integer> set4 = Sets.intersection(set1,set2);
+        Set<Integer> set5 = Sets.difference(set1,set2);
+        Set<Integer> set6 = Sets.symmetricDifference(set1,set2);
+        Set<Integer> set7 = Sets.symmetricDifference(set2,set1);
+        System.out.println(set1);
+        System.out.println(set2);
+        System.out.println(set3);
+        System.out.println(set4);
+        System.out.println(set5);
+        System.out.println(set6);
+        System.out.println(set7);
     }
 }
